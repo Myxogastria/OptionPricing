28b1f97f7d4cfb38303b9756c6f96f122ef099b8
Myxogastria <50288647+Myxogastria@users.noreply.github.com>
2020-04-21 03:02:25 +0900

    crash with const LPVARIANT&

--- OptionPricing/OptionPricing.cpp
+++ OptionPricing/OptionPricing.cpp
@@ -2,5 +2,21 @@
 #include "OptionPricing.h"
 
 OPTIONPRICING_API double WINAPI OptionPrice(const LPVARIANT& argNames, const LPVARIANT& argValues) {
+	MessageBox(NULL, L"OptionPrice", L"DLL", MB_OK);
+	if (!argNames) {
+		MessageBox(NULL, L"argNames is NULL", L"DLL", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	if (!argValues) {
+		MessageBox(NULL, L"argValues is NULL", L"DLL", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	// with const ref, argNames->vt seems to crash on Excel
+	if (argNames->vt) {
+		MessageBox(NULL, L"argNames->vt is not NULL", L"DLL", MB_OK);
+	}
+	else {
+		MessageBox(NULL, L"argNames->vt is NULL", L"DLL", MB_OK);
+	}
 	return 1.0;
 }

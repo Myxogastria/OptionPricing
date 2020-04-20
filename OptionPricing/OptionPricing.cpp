2ba12f95966046646dc334bf232dd12a1736dd33
Myxogastria <50288647+Myxogastria@users.noreply.github.com>
2020-04-21 03:53:32 +0900

    tests of vt

--- OptionPricing/OptionPricing.cpp
+++ OptionPricing/OptionPricing.cpp
@@ -2,5 +2,44 @@
 #include "OptionPricing.h"
 
 OPTIONPRICING_API double WINAPI OptionPrice(LPVARIANT argNames, LPVARIANT argValues) {
-	return 1.0;
+	if (!argNames) {
+		MessageBox(NULL, L"argNames is NULL", L"DLL", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	if (!argNames) {
+		MessageBox(NULL, L"argValues is NULL", L"DLL", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	switch (argNames->vt)
+	{
+	case VT_DISPATCH:
+		MessageBox(NULL, L"argNames->vt is VT_DISPATCH", L"DLL", MB_OK);
+		break;
+	default:
+		break;
+	}
+	if (argNames->vt & VT_ARRAY) {
+		MessageBox(NULL, L"argNames->vt is VT_ARRAY", L"DLL", MB_OK);
+	}
+	else {
+		MessageBox(NULL, L"argNames->vt is not VT_ARRAY", L"DLL", MB_OK);
+	}
+
+	switch (argValues->vt)
+	{
+	case VT_DISPATCH:
+		MessageBox(NULL, L"argValues->vt is VT_DISPATCH", L"DLL", MB_OK);
+		break;
+	default:
+		break;
+	}
+	if (argValues->vt & VT_ARRAY) {
+		MessageBox(NULL, L"argValues->vt is VT_ARRAY", L"DLL", MB_OK);
+	}
+	else {
+		MessageBox(NULL, L"argValues->vt is not VT_ARRAY", L"DLL", MB_OK);
+	}
+
+	return 0.0 + argNames->vt + argValues->vt;
 }

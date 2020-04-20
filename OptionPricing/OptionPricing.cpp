83a0ab446567e0f4b72f1d29b62c86708f2bca79
Myxogastria <50288647+Myxogastria@users.noreply.github.com>
2020-04-21 03:24:21 +0900

    add an argument without const ref

--- OptionPricing/OptionPricing.cpp
+++ OptionPricing/OptionPricing.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "OptionPricing.h"
 
-OPTIONPRICING_API double WINAPI OptionPrice(LPVARIANT lpv) {
+OPTIONPRICING_API double WINAPI OptionPrice(LPVARIANT argNames, LPVARIANT argValues) {
 	return 1.0;
 }
--- OptionPricing/OptionPricing.h
+++ OptionPricing/OptionPricing.h
@@ -2,5 +2,5 @@
 
 extern "C" {
 #define OPTIONPRICING_API __declspec(dllexport)
-	OPTIONPRICING_API double WINAPI OptionPrice(LPVARIANT lpv);
+	OPTIONPRICING_API double WINAPI OptionPrice(LPVARIANT argNames, LPVARIANT argValues);
 }
